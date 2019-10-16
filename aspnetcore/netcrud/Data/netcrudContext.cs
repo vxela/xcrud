87ab195d2aaa9aa37083f1da53679df2cededff2
Jean <jeanccald@gmail.com>
2019-10-16 10:22:09 -0400

    Add aspnetcore framework

---
+++ aspnetcore/netcrud/Data/netcrudContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using mycrud.Models;
+
+namespace netcrud.Models
+{
+    public class netcrudContext : DbContext
+    {
+        public netcrudContext (DbContextOptions<netcrudContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<mycrud.Models.Person> Person { get; set; }
+    }
+}
---
+++ aspnetcore/netcrud/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mycrud.Models
+{
+    public class Person
+    {        
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Phone { get; set; }
+        public int Age { get; set; }
+    }
+}
