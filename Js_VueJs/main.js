5c14c0b2ff00dc7732873f0a478191842760b96f
Juan Hiciano Tineo <juan@advansys.local>
2019-10-22 17:03:56 -0400

    adding vuejs crud

---
+++ Js_VueJs/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import i18n from './locales/index'
+import router from './router'
+import store from './store'
+import './plugins/http'
+import './plugins/vuetify'
+import './plugins/ie'
+import './plugins/custom/'
+import App from './App.vue'
+
+Vue.http.interceptors.push((request, next) => {
+  if (localStorage.getItem('token')) {
+    request.headers.set('Authorization', 'Bearer ' + localStorage.getItem('token'))
+  }
+  next(response => {
+    if (response.status === 400 || response.status === 401 || response.status === 403) {
+      store.commit('auth/logout')
+      router.push({ path: '/login' })
+    }
+  })
+})
+
+new Vue({
+  i18n,
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
